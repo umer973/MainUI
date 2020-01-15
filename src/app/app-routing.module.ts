2a87c692723950126a3f3243ab1247d725a410ee
khanmuzaffer18913 <khanmuzaffer18913@gmail.com>
2020-01-15 23:52:43 +0530

    changes in routin module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,6 +5,10 @@ import { LoginComponent } from './login/login.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { AddProductComponent } from './Project/IGMC/add-product/add-product.component';
 import { LayoutComponent } from './layout/layout/layout.component';
+import { AddBrandComponent } from './Project/IGMC/add-brand/add-brand.component';
+import { AddCategoryComponent } from './Project/IGMC/add-category/add-category.component';
+import { AddDealerComponent } from './Project/IGMC/add-dealer/add-dealer.component';
+import { AddHSNComponent } from './Project/IGMC/add-hsn/add-hsn.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/login', pathMatch: 'full' },
@@ -13,6 +17,11 @@ const routes: Routes = [
     path: 'layout', component: LayoutComponent,
     children: [
       { path: 'addproduct', component: AddProductComponent },
+      { path: 'addbrand', component: AddBrandComponent },
+      { path: 'addcategory', component: AddCategoryComponent },
+      { path: 'adddealer', component: AddDealerComponent },
+      { path: 'addhsn', component: AddHSNComponent },
+      { path: 'dash', component: DashboardComponent },
       { path: 'dash', component: DashboardComponent },
 
     ]
